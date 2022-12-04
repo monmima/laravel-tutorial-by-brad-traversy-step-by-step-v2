0352224492897e2b586e1d2113d39ca013a51416
monmima <monmima@protonmail.com>
2022-12-03 19:14:36 -0500

    add flash confirmation message

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -12,6 +12,7 @@
       crossorigin="anonymous"
       referrerpolicy="no-referrer"
     />
+    <script src="//unpkg.com/alpinejs" defer></script>
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
       tailwind.config = {
@@ -61,5 +62,7 @@
         >Post Job</a
       >
     </footer>
+
+    <x-flash-message />
 </body>
 </html>
